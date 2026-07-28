45bc0430f783614fc83eadb702eeebbadccbfbf7
Viscora-Game <viscoragames@gmail.com>
2026-07-29 00:18:58 +0300

    Deploy versionCode 7 / v6.4.0 digitally signed AAB release bundle for Google Play Console submission

---
+++ twa_project/app/src/main/java/com/viscoragame/eslegitsin3d/DelegationService.java
@@ -0,0 +1,14 @@
+package com.viscoragame.eslegitsin3d;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
